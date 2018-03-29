0cdf247a0a5e516b04e208e253141b7bb269a7bd
Jack Humbert <jack.humb@gmail.com>
2018-03-29 16:18:54 -0400

    add travis script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+rm -fdr .git
+git init
+git remote add origin config https://github.com/${TRAVIS_REPO_SLUG}
+git pull origin config
+
+rm -fdr .git
+git init
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "QMK Bot"
+git config user.email "hello@qmk.fm"
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+# Force push from the current repo's dev branch to the remote github.io
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:gh-pages > /dev/null 2>&1
