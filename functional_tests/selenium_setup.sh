41b868a8b2011ec02e82a1c23c263dcdeab69f5a
mechmerlin <mechmerlin@gmail.com>
2018-07-23 12:30:18 -0700

    Run the script through shell check Add password prompt if no sudo as advised by skully

--- functional_tests/selenium_setup.sh
+++ functional_tests/selenium_setup.sh
@@ -4,9 +4,8 @@
 # Installs Chrome Web Driver version 2.40
 # Installs Selenium Standalone Server version 3.13.0
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root (with sudo command)"
-  exit
+if [ "$EUID" -ne 0 ]; then 
+    exec sudo "$0" $@; 
 fi
 
 # Determine which OS
@@ -32,30 +31,30 @@ fi
 
 # Download Chrome Web Driver .zip file to temporary location
 TEMP_PATH=${PWD}"/tmp/chromedriver"
-mkdir $TEMP_PATH && pushd $TEMP_PATH
-wget $CHR_DRIVER -P $TEMP_PATH
-printf "Chrome Driver zip file downloaded to ${TEMP_PATH}.\n"
+mkdir "$TEMP_PATH" && pushd "$TEMP_PATH"
+wget $CHR_DRIVER -P "$TEMP_PATH"
+printf "Chrome Driver zip file downloaded to %s.\\n" "${TEMP_PATH}"
 
 # Unzip Chrome Driver zip file to qmk_configurator/functional_tests/bin directory
 DEST_PATH=${PWD}"/bin"
-unzip ${TEMP_PATH}/chromedriver_$DRIVER.zip
-sudo mv -f ./chromedriver $DEST_PATH
-sudo chown root:root $DEST_PATH
-sudo chmod 0755 $DEST_PATH
+unzip "${TEMP_PATH}"/chromedriver_$DRIVER.zip
+sudo mv -f ./chromedriver "$DEST_PATH"
+sudo chown root:root "$DEST_PATH"
+sudo chmod 0755 "$DEST_PATH"
 
-printf "Installed Chrome Web Driver for ${DRIVER} to ${DEST_PATH}.\n"
+printf "Installed Chrome Web Driver for %s to %s.\\n" "${DRIVER}" "${DEST_PATH}"
 
 #Download and install Standalone Selenium Server
 SELENIUM_VERSION="3.13.0"
 SELENIUM_JAR_FILE=selenium-server-standalone-$SELENIUM_VERSION.jar
-wget http://selenium-release.storage.googleapis.com/$(echo $SELENIUM_VERSION | cut -d'.' -f-2)/$SELENIUM_JAR_FILE -P $TEMP_PATH
-mv -f $TEMP_PATH/$SELENIUM_JAR_FILE $DEST_PATH
-chown root:root $DEST_PATH$SELENIUM_JAR_FILE
-chmod 755 $DEST_PATH$SELENIUM_JAR_FILE
+wget http://selenium-release.storage.googleapis.com/$(echo "$SELENIUM_VERSION" | cut -d'.' -f-2)/$SELENIUM_JAR_FILE -P "$TEMP_PATH"
+mv -f "$TEMP_PATH/$SELENIUM_JAR_FILE" "$DEST_PATH"
+chown root:root "$DEST_PATH$SELENIUM_JAR_FILE"
+chmod 755 "$DEST_PATH$SELENIUM_JAR_FILE"
 
-printf "Installed Selenium Standalone Server ${SELENIUM_VERSION}.\n"
+printf "Installed Selenium Standalone Server %s. \\n" "${SELENIUM_VERSION}"
 
-printf "Deleting directory qmk_configurator/functional_tests/tmp \n"
+printf "Deleting directory qmk_configurator/functional_tests/tmp \\n"
 sudo rm -rf tmp/
 
-printf "Installation is successful.\n"
+printf "Installation is successful. \\n"
