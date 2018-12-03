c383d42223fd34fbe2b98f36845a02f560e2a952
Yan-Fa Li <yanfali@gmail.com>
2018-12-02 21:21:24 -0800

    Add StatusPanel

---
+++ configurator/src/components/StatusPanel.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="status">
+    <textarea
+      id="terminal"
+      v-model="message"
+      ref="terminal"
+      readonly>
+    </textarea>
+  </div>
+</template>
+<script>
+import { mapGetters, mapMutations } from 'vuex';
+export default {
+  name: 'status-panel',
+  watch: {
+    message(newV, oldV) {
+      if (this.scrollToLatest && newV !== oldV) {
+        this.scrollToEnd();
+        this.doneScroll();
+      }
+    }
+  },
+  methods: {
+    ...mapMutations('status', ['doneScroll']),
+    scrollToEnd() {
+      let terminal = this.$refs.terminal;
+      this.$nextTick(() => {
+        terminal.scrollTop = terminal.scrollHeight;
+      });
+    }
+  },
+  computed: {
+    ...mapGetters('status', ['message', 'scrollToLatest'])
+  },
+  data: () => {
+    return {};
+  }
+};
+</script>
