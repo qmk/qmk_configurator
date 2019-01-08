b9fcd80c852efef9a33027b934363f34fa0e6fee
Yan-Fa Li <yanfali@gmail.com>
2019-01-08 11:48:17 -0800

    tabbed keycodes

--- configurator/src/components/Keycodes.vue
+++ configurator/src/components/Keycodes.vue
@@ -18,14 +18,28 @@
         </span>
       </p>
     </div>
+
     <div id="keycodes">
-      <template v-for="(key, index) in keycodes">
-        <component
-          v-bind:is="getComponent(key.code)"
-          v-bind="key"
+      <div class="tabs">
+        <span
+          class="tab"
+          :class="classes(index)"
+          v-for="(key, index) in keycodesByLabel"
           :key="index"
-        />
-      </template>
+          @click="changeActive(index)"
+        >
+          {{ index }}
+        </span>
+      </div>
+      <div class="tab-area">
+        <template v-for="(key, index) in activeTab">
+          <component
+            v-bind:is="getComponent(key.code)"
+            v-bind="key"
+            :key="index"
+          />
+        </template>
+      </div>
     </div>
   </div>
 </template>
@@ -40,21 +54,82 @@ export default {
   components: { Keycode, Space },
   props: {},
   data() {
-    return {};
+    return {
+      active: 'Standard'
+    };
   },
   computed: {
-    ...mapGetters('keycodes', ['keycodes'])
+    ...mapGetters('keycodes', ['keycodes']),
+    activeTab() {
+      return this.keycodesByLabel[this.active];
+    },
+    keycodesByLabel() {
+      let section = this.keycodes.reduce((acc, value) => {
+        if (value.label) {
+          acc[value.label] = [];
+          acc.current = value.label;
+        } else {
+          acc[acc.current].push(value);
+        }
+        return acc;
+      }, {});
+      delete section['current'];
+      return section;
+    }
   },
   methods: {
     getComponent(code) {
       return isUndefined(code) ? Space : Keycode;
+    },
+    classes(current) {
+      let classes = [];
+      if (current === this.active) {
+        classes.push('active');
+      }
+      return classes.join(' ');
+    },
+    changeActive(index) {
+      this.active = index;
     }
   }
 };
 </script>
 <style>
-.keycodes {
+#keycodes {
+  background: #fff;
+  border: none;
+  margin-bottom: 20px;
+}
+.tabs {
   display: grid;
-  grid-template: repeat(auto-fill, 3px) / repeat(auto-fill, 3px);
+  background: #fff;
+  grid-template: auto / repeat(5, minmax(100px, 200px));
+}
+.tab {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  border-top: 1px solid #ccc;
+  border-left: 1px solid #ccc;
+  border-right: 1px solid #ccc;
+  padding: 2px;
+  margin-right: 2px;
+  border-radius: 8px 4px 0 0;
+  background: #eee;
+  display: inline-block;
+  opacity: 0.55;
+  z-index: 100;
+  cursor: pointer;
+}
+.tab-area {
+  height: 300px;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-top: 0;
+  background: #eee;
+}
+.tab.active {
+  opacity: 1;
+  background: #eee;
 }
 </style>
