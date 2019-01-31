520ad3111ea51dd26db6a3bd422ab1a69528c186
Yan-Fa Li <yanfali@gmail.com>
2019-01-31 10:45:14 -0800

    Organize tests better

--- configurator/tests/e2e/specs/test.js
+++ configurator/tests/e2e/specs/test.js
@@ -1,17 +1,11 @@
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
-
 module.exports = {
   'default e2e tests': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
       .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.main')
-      .assert.containsText('.topctrl-1-1', 'KEYBOARD:')
-      .assert.containsText('.topctrl-2-1', 'LAYOUT:')
-      .assert.containsText('.topctrl-1-2', 'KEYMAP NAME:')
-      .assert.containsText('#load-default', 'LOAD DEFAULT')
-      .assert.containsText('#compile', 'COMPILE')
       .end();
   }
 };
