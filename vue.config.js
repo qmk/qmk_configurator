5ba66ed775cb20432999606513a6e11a0f7eb111
Yan-Fa Li <yanfali@gmail.com>
2019-03-30 12:42:17 -0700

    Make google analytics build smarter
     - warn ga-production.js is missing if doing a build

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,25 @@
 const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+
+global.appRoot = path.resolve(__dirname);
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '.' : '/',
   configureWebpack: config => {
-    config.plugins.push(
-      new webpack.NormalModuleReplacementPlugin(
-        /\.\/ga-development/,
-        './ga-production'
-      )
-    );
+    if (process.env.NODE_ENV === 'production') {
+      if (fs.existsSync(global.appRoot + '/ga-production.js')) {
+        console.log(global.appRoot);
+        config.plugins.push(
+          new webpack.NormalModuleReplacementPlugin(
+            /\.\/ga-development/,
+            './ga-production'
+          )
+        );
+      } else {
+        console.warn(
+          'WARNING: no ga-production.js file found. Using development'
+        );
+      }
+    }
   }
 };
