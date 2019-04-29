06c1ec96b1a7e2ce46f03dea47fbc52cbe502870
Yan-Fa Li <yanfali@gmail.com>
2019-04-28 18:56:21 -0700

    Add a print route

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Print from './views/Print.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     { path: '/:keyboardP(.+)/:layoutP(.+)', component: Home, name: 'home' },
-    { path: '/', component: Home, name: 'home' }
+    { path: '/', component: Home, name: 'home' },
+    { path: '/print', component: Print, name: 'print' }
   ]
 });
---
+++ src/views/Print.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>Printable View</h3>
+    <button @click="gohome">Configurator</button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+  name: 'printerator',
+  computed: {
+    ...mapState('app', ['keyboard', 'layout'])
+  },
+  components: {},
+  methods: {
+    gohome() {
+      this.$router.push(`/${this.keyboard}/${this.layout}`);
+    }
+  }
+};
+</script>
+<style></style>
