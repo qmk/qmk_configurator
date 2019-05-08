461d2d344e2320f6a7f4b22b9798f1b75e2bb23a
noroadsleft <xxiinophobia@yahoo.com>
2019-05-08 09:59:57 -0700

    Fix indentation in Oblivion Hagoromo JS file

--- src/components/colorways/sa_oblivion_hagoromo.js
+++ src/components/colorways/sa_oblivion_hagoromo.js
@@ -1,8 +1,8 @@
 export default {
   name: 'sa-oblivion-hagoromo',
   override: {
-	KC_ESC: 'accent',
-	KC_ENT: 'accent',
-	KC_PENT: 'accent'
+    KC_ESC: 'accent',
+    KC_ENT: 'accent',
+    KC_PENT: 'accent'
   }
 };
