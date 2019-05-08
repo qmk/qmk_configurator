c8d1e2be9232d6e7349b0dfaa0e19b5dd137442c
noroadsleft <xxiinophobia@yahoo.com>
2019-05-08 10:04:09 -0700

    Add complete Numpad numeric keycodes to alphaCodes.js

--- src/components/colorways/alphaCodes.js
+++ src/components/colorways/alphaCodes.js
@@ -12,7 +12,17 @@ export default {
     'KC_F11',
     'KC_F12',
     'KC_BSLS',
-    'KC_P0'
+    'KC_P0',
+    'KC_P1',
+    'KC_P2',
+    'KC_P3',
+    'KC_P4',
+    'KC_P5',
+    'KC_P6',
+    'KC_P7',
+    'KC_P8',
+    'KC_P9',
+    'KC_PDOT'
   ].reduce((acc, code) => {
     acc[code] = true;
     return acc;
