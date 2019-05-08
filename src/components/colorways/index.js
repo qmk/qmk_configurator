67da280ee744f71f3884623173ea8df123c1343b
noroadsleft <xxiinophobia@yahoo.com>
2019-05-08 08:33:40 -0700

    Enable accent-colored keys for SA Danger Zone

--- src/components/colorways/index.js
+++ src/components/colorways/index.js
@@ -11,7 +11,25 @@ export default {
   list: [
     dsa_galaxy_class,
     sa_carbon,
-    { name: 'sa-danger-zone' },
+    {
+      name: 'sa-danger-zone',
+      override: {
+        KC_ESC: 'accent-red',
+        KC_ENT: 'accent-yellow',
+        KC_F1: 'key',
+        KC_F2: 'key',
+        KC_F3: 'key',
+        KC_F4: 'key',
+        KC_F9: 'key',
+        KC_F10: 'key',
+        KC_F11: 'key',
+        KC_F12: 'key',
+        KC_UP: 'key',
+        KC_LEFT: 'key',
+        KC_DOWN: 'key',
+        KC_RGHT: 'key'
+      }
+    },
     {
       name: 'sa-jukebox',
       override: {
