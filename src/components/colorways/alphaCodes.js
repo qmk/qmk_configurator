eb3c75e943ea7260ff84f186ebbbf2dac7f14eab
noroadsleft <xxiinophobia@yahoo.com>
2019-05-08 08:33:35 -0700

    Change keycode type assignments
    Assign to alpha coloring:
    
    - KC_F1
    - KC_F2
    - KC_F3
    - KC_F4
    - KC_F9
    - KC_F10
    - KC_F11
    - KC_F12
    - KC_BSLS
    
    Also moved KC_ESC to the top of the mod code list because I disliked where it was.

--- src/components/colorways/alphaCodes.js
+++ src/components/colorways/alphaCodes.js
@@ -3,6 +3,15 @@ export default {
    * List of keycodes that should always be considered alphas
    */
   ... [
+    'KC_F1',
+    'KC_F2',
+    'KC_F3',
+    'KC_F4',
+    'KC_F9',
+    'KC_F10',
+    'KC_F11',
+    'KC_F12',
+    'KC_BSLS',
     'KC_P0'
   ].reduce((acc, code) => {
     acc[code] = true;
