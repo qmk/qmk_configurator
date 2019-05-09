ecff1524d812f45f17dc908d826722a579c96f4e
Yan-Fa Li <yanfali@gmail.com>
2019-05-09 12:38:48 -0700

    Index colorways by directory (#341)
    * Index colorways by directory
    
     - reduces import noise at top level
     - hides complexity of imports in directory
    
    * Update src/components/colorways/index.js
    
    Co-Authored-By: noroadsleft <18669334+noroadsleft@users.noreply.github.com>

--- src/components/colorways/index.js
+++ src/components/colorways/index.js
@@ -1,54 +1,13 @@
 import dsa_galaxy_class from './dsa/galaxy_class';
-import sa_carbon from './sa/carbon';
-import sa_danger_zone from './sa/danger_zone';
-import sa_jukebox from './sa/jukebox';
-import sa_modern_selectric from './sa/modern_selectric';
-import sa_nantucket_selectric from './sa/nantucket_selectric';
-import sa_oblivion_hagoromo from './sa/oblivion_hagoromo';
-import gmk_dolch from './gmk/dolch';
-import gmk_jamon from './gmk/jamon';
-import gmk_merlin from './gmk/merlin';
-import gmk_metaverse from './gmk/metaverse';
-import gmk_mizu from './gmk/mizu';
-import gmk_nautilus from './gmk/nautilus';
-import gmk_olivetti from './gmk/olivetti';
-import gmk_olivia from './gmk/olivia';
-import gmk_serika from './gmk/serika';
-import gmk_space_cadet from './gmk/space_cadet';
-import gmk_striker from './gmk/striker';
-import gmk_ta_royal_alpha from './gmk/ta_royal_alpha';
-import gmk_wob from './gmk/wob';
-import sa_vilebloom from './sa/vilebloom';
+import gmk from './gmk';
+import sa from './sa';
 import alphaCodes from './alphaCodes';
 import modCodes from './modCodes';
 
 export default {
   alphaCodes,
   modCodes,
-  list: [
-    dsa_galaxy_class,
-    sa_carbon,
-    sa_danger_zone,
-    sa_jukebox,
-    sa_modern_selectric,
-    sa_nantucket_selectric,
-    sa_oblivion_hagoromo,
-    sa_vilebloom,
-
-    gmk_dolch,
-    gmk_jamon,
-    gmk_merlin,
-    gmk_metaverse,
-    gmk_mizu,
-    gmk_nautilus,
-    gmk_olivetti,
-    gmk_olivia,
-    gmk_serika,
-    gmk_space_cadet,
-    gmk_striker,
-    gmk_ta_royal_alpha,
-    gmk_wob
-  ],
+  list: [dsa_galaxy_class, ...sa, ...gmk],
   /*
    * List of codes we should use icons for instead of text
    */
---
+++ src/components/colorways/sa/index.js
@@ -0,0 +1,17 @@
+import sa_carbon from './carbon';
+import sa_danger_zone from './danger_zone';
+import sa_jukebox from './jukebox';
+import sa_modern_selectric from './modern_selectric';
+import sa_nantucket_selectric from './nantucket_selectric';
+import sa_oblivion_hagoromo from './oblivion_hagoromo';
+import sa_vilebloom from './vilebloom';
+
+export default [
+  sa_carbon,
+  sa_danger_zone,
+  sa_jukebox,
+  sa_modern_selectric,
+  sa_nantucket_selectric,
+  sa_oblivion_hagoromo,
+  sa_vilebloom
+];
