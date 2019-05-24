cb12778051155efdf7354ab3567620dd91e2444f
heieisch <50953609+heieisch@users.noreply.github.com>
2019-05-24 08:43:40 +0200

    German Translation
    * German Translation
    
    created copy of english index.js and potato.js
    translated content
    
    added the Files in the Import File

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,9 +2,11 @@ import en from './en';
 import ja from './ja';
 import zh from './zh';
 import fr from './fr';
+import de from './de';
 export default {
   ...en,
   ...ja,
+  ...de,
   ...fr,
   ...zh
 };
