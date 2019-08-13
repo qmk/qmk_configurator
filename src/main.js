1d1c1bfcacdc38ddee1dcf9cd974b5d1752dfbde
Elliot Powell <johnp8727@gmail.com>
2019-08-14 00:10:19 +0100

    minor change to status append

--- src/main.js
+++ src/main.js
@@ -55,6 +55,7 @@ if (
   window.electron = true; //We set a global value to be used later
   //We use the Bridge as a way to share functions between electron and vue
   window.Bridge.statusAppend = txt => {
+    txt = '\n' + txt;
     store.commit('status/append', txt);
     store.dispatch('status/scrollToEnd');
   };
