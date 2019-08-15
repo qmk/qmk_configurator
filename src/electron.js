496e5f904359aa64517b85b1394dfda89b0b260d
Elliot Powell <johnp8727@gmail.com>
2019-08-15 23:24:11 +0100

    fix exports

--- src/electron.js
+++ src/electron.js
@@ -1,7 +1,7 @@
 import store from './store';
 
-module.exports = {
-  initElectron: () => {
+export default {
+  init() {
     //We use the Bridge as a way to share functions between electron and vue
     window.Bridge.statusAppend = txt => {
       txt = '\n' + txt;
