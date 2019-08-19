7a0ac98495cb0ad0ce51054ee071b664175be900
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-08-19 23:12:50 +0200

    review

--- tests/integration/start.spec.js
+++ tests/integration/start.spec.js
@@ -11,17 +11,27 @@ describe('Simple browsing', function() {
       '2_milk',
       '30wer'
     ]);
-  });
-  it('Starts the app Check the dom', function() {
     cy.visit('/');
+  });
+  it('Top Controller should be instanciated and visible', function() {
     cy.get('#controller-top').should('be.visible');
+  });
+  it('Visual Keymap should be visible', function() {
     cy.get('#visual-keymap').should('be.visible');
+  });
+  it('Terminal should be visible', function() {
     cy.get('#status').should('be.visible');
+  });
+  it('Keycode section should be present', function() {
     cy.get('#keycodes-section').should('be.visible');
+  });
+  it('Side panel should display and disappear properly', function() {
     cy.get('.openSettings').click();
     cy.get('.settings-panel', { timeout: 5000 }).should('be.visible');
     cy.get('.slideout-panel-bg').click();
+  });
+  it('Tester should be accessible and instanciated', function() {
     cy.get('#testkeys').click();
-    cy.get('.visual-tester-keymap').should('be.visible');
+    cy.get('.visual-tester-keymap', { timeout: 5000 }).should('be.visible');
   });
 });
