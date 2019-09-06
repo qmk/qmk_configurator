82ff156bd937801cbbbb56cc2ae5ee44184e7a86
gab.a <gab.a@users.noreply.github.com>
2019-09-06 17:49:05 -0400

    French Translation on the new feature
    added french translation for the new features and buttons

---
+++ src/i18n/fr/print.js
@@ -0,0 +1,35 @@
+export default {
+  keyboard: {
+    label: 'Clavier'
+  },
+  layout: {
+    label: 'Disposition'
+  },
+  layer: {
+    label: 'Calque'
+  },
+  anonymous: {
+    label: 'Anonyme'
+  },
+  author: {
+    title: 'Auteur',
+    placeholder: 'Votre nom (Optionnel)'
+  },
+  date: {
+    title: 'Date'
+  },
+  source: {
+    title: 'Source'
+  },
+  print: {
+    title: 'Imprimer'
+  },
+  back: {
+    title: 'Retour'
+  },
+  notes: {
+    title: 'Notes',
+    placeholder: 'Notes sur la configuration',
+    empty: 'Ma configuration'
+  }
+};
