7c5cb97a2801f04d1a83040a9af5eeae780cc165
Yan-Fa Li <yanfali@gmail.com>
2019-09-08 19:00:41 -0700

    Add a more helpful msg when redis is failing
     - use the job count to figure out when we are having service issues and
     display error information

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -8,7 +8,7 @@
       {{ $t('message.apiVersion') }}:
       <span class="version-num">{{ version }}</span>
     </div>
-    <div class="bes-jobs">{{ jobs }}</div>
+    <div class="bes-jobs" :class="jobCountClass">{{ jobs }}</div>
   </div>
 </template>
 <script>
@@ -20,8 +20,22 @@ import { backend_status_url } from '@/store/modules/constants';
  * checkStatus - check status component to poll API for errors and status
  * @return {object} Vue app component
  */
+
+const highWaterMark = 20;
+const warningWaterMark = 10;
 export default {
   name: 'status-bar',
+  computed: {
+    jobCountClass() {
+      if (this.jobCount < warningWaterMark) {
+        return '';
+      }
+      if (this.jobCount < highWaterMark) {
+        return 'bes-odd-job-count';
+      }
+      return 'bes-high-job-count';
+    }
+  },
   methods: {
     getPollInterval() {
       return 25000 + 5000 * Math.random();
@@ -30,15 +44,22 @@ export default {
       axios
         .get(backend_status_url)
         .then(({ data }) => {
-          var localTime = new Date(data.last_ping).toTimeString();
-          var stat = data.status;
-          stat = stat === 'running' ? 'UP' : stat;
-          this.status = escape(`${stat} @ ${localTime}`);
           this.version = data.version;
+          this.jobCount = parseInt(data.queue_length, 10);
           this.jobs = template(
-            `<%= queue_length %> ${this.$t('message.jobsWaiting')}`
-          )(data);
-          this.hasError = false;
+            `<%= jobCount %> ${this.$t('message.jobsWaiting')}`
+          )(this);
+          if (this.jobCount < highWaterMark) {
+            var localTime = new Date(data.last_ping).toTimeString();
+            var stat = data.status;
+            stat = stat === 'running' ? 'UP' : stat;
+            this.status = escape(`${stat} @ ${localTime}`);
+            this.hasError = false;
+          } else {
+            this.status =
+              'Redis is probably down. Please contact devs on QMK discord';
+            this.hasError = true;
+          }
         })
         .catch(json => {
           var localTime = new Date().toTimeString();
@@ -54,6 +75,7 @@ export default {
       status: 'Checking',
       version: '0.1',
       jobs: '...',
+      jobCount: 0,
       hasError: false
     };
   },
@@ -62,3 +84,11 @@ export default {
   }
 };
 </script>
+<style>
+.bes-high-job-count {
+  color: red;
+}
+.bes-odd-job-count {
+  color: yellow;
+}
+</style>
