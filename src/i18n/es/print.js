aeeb1cd00e8e0301736042cc73a7ddcb217f306b
Karey Higuera <karey.higuera@gmail.com>
2019-10-04 11:20:37 -0500

    Add Spanish translation Update index.js with prettier

---
+++ src/i18n/es/print.js
@@ -0,0 +1,35 @@
+export default {
+  keyboard: {
+    label: 'Teclado'
+  },
+  layout: {
+    label: 'Plano'
+  },
+  layer: {
+    label: 'Capa'
+  },
+  anonymous: {
+    label: 'Anónimo'
+  },
+  author: {
+    title: 'Autor',
+    placeholder: 'Opcionalmente, Tu Nombre'
+  },
+  date: {
+    title: 'Fecha'
+  },
+  source: {
+    title: 'Fuente'
+  },
+  print: {
+    title: 'Imprimir'
+  },
+  back: {
+    title: 'Atrás'
+  },
+  notes: {
+    title: 'Notas',
+    placeholder: 'Notas sobre esta configuración',
+    empty: 'Mi keymap genial'
+  }
+};
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,10 +3,12 @@ import ja from './ja';
 import zh from './zh';
 import fr from './fr';
 import de from './de';
+import es from './es';
 export default {
   ...en,
   ...ja,
   ...de,
   ...fr,
-  ...zh
+  ...zh,
+  ...es
 };
