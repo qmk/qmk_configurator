1d125d9dc4f59286c6747a14fbcd57e73475fc42
Karey Higuera <karey.higuera@gmail.com>
2019-10-04 12:24:58 -0500

    Re-order languages keys to be alphabetical

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,14 +1,14 @@
+import de from './de';
 import en from './en';
+import es from './es';
+import fr from './fr';
 import ja from './ja';
 import zh from './zh';
-import fr from './fr';
-import de from './de';
-import es from './es';
 export default {
-  ...en,
-  ...ja,
   ...de,
+  ...en,
+  ...es,
   ...fr,
-  ...zh,
-  ...es
+  ...ja,
+  ...zh
 };
