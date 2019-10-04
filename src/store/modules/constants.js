6367c97ab72acc5ba0faa24509ffeb6a1ea7f6ad
Vincent LE GOFF <g_n_s@hotmail.fr>
2019-10-05 00:00:52 +0200

    add env variable handling for API url (#518)
    Thank you

--- src/store/modules/constants.js
+++ src/store/modules/constants.js
@@ -1,6 +1,9 @@
 import template from 'lodash/template';
 
-export const backend_baseurl = 'https://api.qmk.fm';
+export const backend_baseurl = process.env.VUE_APP_API_URL;
+if (!backend_baseurl) {
+  throw 'Backend URL has not been specified';
+}
 export const backend_keyboards_url = `${backend_baseurl}/v1/keyboards`;
 export const backend_compile_url = `${backend_baseurl}/v1/compile`;
 export const backend_status_url = `${backend_baseurl}/v1`;
