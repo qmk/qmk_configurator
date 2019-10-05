a96c7d7ecfdf9e426d7c818fbd2db4a1601f466b
StrikerRUS <nekit94-12@hotmail.com>
2019-10-05 03:08:09 +0300

    added Russian translation

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,6 +3,7 @@ import en from './en';
 import es from './es';
 import fr from './fr';
 import ja from './ja';
+import ru from './ru';
 import zh from './zh';
 export default {
   ...de,
@@ -10,5 +11,6 @@ export default {
   ...es,
   ...fr,
   ...ja,
+  ...ru,
   ...zh
 };
