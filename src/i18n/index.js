6c2dcde315c9783e178cf53cdf41d3cc576d1096
bantya <16685565+bantya@users.noreply.github.com>
2019-10-22 01:48:07 +0530

    Fixed language codes and import satements

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,17 +2,21 @@ import de from './de';
 import en from './en';
 import es from './es';
 import fr from './fr';
+import hi from './hi';
 import ja from './ja';
-import ru from './ru';
-import zh from './zh';
+import mr from './mr';
 import plPL from './pl-PL';
 import ptBR from './pt-BR';
+import ru from './ru';
+import zh from './zh';
 export default {
   ...de,
   ...en,
   ...es,
   ...fr,
+  ...hi,
   ...ja,
+  ...mr,
   ...plPL,
   ...ptBR,
   ...ru,
--- src/i18n/mr/index.js
+++ src/i18n/mr/index.js
@@ -2,7 +2,7 @@ import potato from './potato';
 import print from './print';
 import tester from './tester';
 export default {
-  en: {
+  mr: {
     message: {
       ...potato,
       help: {
