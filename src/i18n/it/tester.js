0bf652b97994f732f8042c32968fbf76d4f7249c
Michael Zhou <myz227@nyu.edu>
2019-10-26 13:00:54 -0400

    Added Italian translation

---
+++ src/i18n/it/tester.js
@@ -0,0 +1,35 @@
+export default {
+  page: {
+    label: 'Tastierista'
+  },
+  keycodeStatus: {
+    label: 'Codici chiave rilevati'
+  },
+  back: {
+    label: 'Indietro',
+    title: 'Torna al Configuratore'
+  },
+  reset: {
+    label: 'Reimpostato',
+    title: 'Reimposta le chiavi rilevate'
+  },
+  letters: {
+    keycode: {
+      label: 'codici chiave'
+    },
+    code: {
+      label: 'codice'
+    },
+    key: {
+      label: 'chiave'
+    }
+  },
+  docs: {
+    paragraph:
+      'Nota: chiave e codice rilevati possono essere diversi a seconda della vostra posizione. Doc'
+  },
+  chatter: {
+    label: 'Soglia di chatter (ms)',
+    detectedAlert: 'Le comunicazioni sono state rilevate!'
+  }
+};
