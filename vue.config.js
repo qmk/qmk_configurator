be29687ab8811697d3ed97129778b7e119bbd998
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-11-24 11:41:46 +0100

    cleanup

--- vue.config.js
+++ vue.config.js
@@ -5,21 +5,5 @@ const fs = require('fs');
 global.appRoot = path.resolve(__dirname);
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '.' : '/',
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      if (fs.existsSync(global.appRoot + '/src/ga-production.js')) {
-        console.log(global.appRoot);
-        config.plugins.push(
-          new webpack.NormalModuleReplacementPlugin(
-            /\.\/ga-development/,
-            './ga-production'
-          )
-        );
-      } else {
-        console.warn(
-          'WARNING: no ga-production.js file found. Using development'
-        );
-      }
-    }
-  }
+  configureWebpack: config => {}
 };
