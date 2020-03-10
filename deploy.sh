74cbe5803b36fe4a9cb030c79b018f00e5a6ea90
Vincent LE GOFF <g_n_s@hotmail.fr>
2020-03-10 01:14:26 +0100

    Add GitHub ci (#594)
    * add build yaml
    
    fallback to deploy.sh
    
    travis delete
    
    * fix

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-yarn lint
-yarn test:unit
-yarn test:e2e
+# yarn lint
+# yarn test:unit
+# yarn test:e2e
 
-if [ "${TRAVIS_PULL_REQUEST}"  != "false" ]; then
-  # Exit if we're running a pull request
-  exit 0;
-fi
+# if [ "${TRAVIS_PULL_REQUEST}"  != "false" ]; then
+#   # Exit if we're running a pull request
+#   exit 0;
+# fi
 
-rm -rf .git
+# rm -rf .git
+# cd dist
+# git init
+# git remote add origin https://github.com/${TRAVIS_REPO_SLUG}
+# git pull origin config
+
+# rm -rf .git
+# git init
+
 cd dist
 git init
-git remote add origin https://github.com/${TRAVIS_REPO_SLUG}
-git pull origin config
-
-rm -rf .git
-git init
+git remote add origin https://github.com/qmk_configurator
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "QMK Bot"
@@ -32,4 +36,4 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@github.com/qmk_configurator" master:gh-pages > /dev/null 2>&1
