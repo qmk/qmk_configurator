ad597814c9b2f02a96cd461a058edc517cfd8fe0
Chris Newton <15189005+christophernewton@users.noreply.github.com>
2020-04-21 13:02:57 +1000

    Add dynamic width to ANY input (fixed PR) (#713)
    * updated ui to change input size on focus
    
    * fixed undefined on focus for anykey

--- src/components/AnyKey.vue
+++ src/components/AnyKey.vue
@@ -13,13 +13,14 @@
     @dragleave.prevent="dragleave"
     @dragover.prevent="dragover"
     @dragenter.prevent="dragenter"
-  ><div>{{ displayName }}<div><input
+  ><div :class="`${hasFocus ? 'key-layer-title-focus' : 'key-layer-title'}`">{{ displayName }}<div><input
   class="key-layer-input"
   @focus.prevent.stop="focus"
   @blur.prevent.stop="blur"
   @click.prevent.stop="clickignore"
   ref="input"
   spellcheck="false"
+  :style="`width:calc(${this.charLength}ch + 6px);`"
   v-model="value" /></div></div><div
         v-if="visible"
         class="remove"
@@ -32,12 +33,19 @@ import BaseKey from './BaseKey';
 export default {
   name: 'any-key',
   extends: BaseKey,
+  data() {
+    return {
+      charLength: 3,
+      hasFocus: false
+    };
+  },
   computed: {
     value: {
       get() {
         return this.meta.text;
       },
       set(value) {
+        this.updateWidth(value);
         this.setText({
           layer: this.$store.state.keymap.layer,
           index: this.id,
@@ -50,11 +58,18 @@ export default {
     ...mapMutations('keymap', ['setText']),
     blur() {
       this.startListening();
+      this.hasFocus = false;
+      this.charLength = 3;
       this.setSelected(undefined);
     },
     focus() {
       this.stopListening();
+      this.hasFocus = true;
+      this.charLength = this.value ? this.value.length : 3;
       this.$refs.input.selectionStart = this.$refs.input.selectionEnd = 1000;
+    },
+    updateWidth(value) {
+      this.charLength = value.length;
     },
     clickignore() {
       this.stopListening();
