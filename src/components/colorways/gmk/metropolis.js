5305a8cc2daa9d969116325ac43d8d841d77250b
Sendy YK <hi@sendyyk.com>
2020-09-22 13:15:57 +0700

    Fix error

--- src/components/colorways/gmk/metropolis.js
+++ src/components/colorways/gmk/metropolis.js
@@ -1,42 +1,42 @@
 export default {
     name: 'gmk-metropolis',
     override: {
-        KC_ESC: 'esc', // Esc
-        KC_GESC: 'esc', //Esc
-        KC_ENT: 'enter', // Enter
-        KC_PENT: 'enter', // Numpad Enter
-        KC_TAB: 'accent1', // Tab
-        KC_CAPS: 'accent1', //  Caps Lock
-        KC_BSPC: 'accent1', // Backspace
-        KC_PSCR: 'accent1', // Print Screen
-        KC_SLCK: 'accent1', // Scroll Lock
-        KC_PAUS: 'accent1', // Pause
-        KC_INS: 'accent1', // Insert
-        KC_HOME: 'accent1', // Home
-        KC_PGUP: 'accent1', // Page Up
-        KC_DEL: 'accent1', // Delete
-        KC_END: 'accent1', // End
-        KC_PGDN: 'accent1', // Page Down
-        KC_NLCK: 'accent1', // Num Lock
-        KC_PSLS: 'accent1', // /
-        KC_PAST: 'accent1', // *
-        KC_PMNS: 'accent1', // -
-        KC_PPLS: 'accent1', // +
-        KC_PCMM: 'accent1', // ,
-        KC_PEQL: 'accent1', // =
-        KC_LSFT: 'accent2', // Left Shift
-        KC_LCTL: 'accent2', // Left Ctrl
-        KC_LGUI: 'accent2', // Left OS
-        KC_LALT: 'accent2', // Left Alt
-        KC_RALT: 'accent2', // Right Alt
-        KC_RGUI: 'accent2', // Right OS
-        KC_APP: 'accent2', // Menu
-        KC_RCTL: 'accent2', // Right Ctrl
-        KC_RSFT: 'accent2', // Right Shift
-        KC_UP: 'accent2', // Up Arrow
-        KC_LEFT: 'accent2', // Left Arrow
-        KC_DOWN: 'accent2', // Down Arrow
-        KC_RGHT: 'accent2', // Right Arrow
-        MO(layer): 'accent3' // Layer
+        KC_ESC: 'esc',
+        KC_GESC: 'esc',
+        KC_ENT: 'enter',
+        KC_PENT: 'enter',
+        KC_TAB: 'accent1',
+        KC_CAPS: 'accent1',
+        KC_BSPC: 'accent1',
+        KC_PSCR: 'accent1',
+        KC_SLCK: 'accent1',
+        KC_PAUS: 'accent1',
+        KC_INS: 'accent1',
+        KC_HOME: 'accent1',
+        KC_PGUP: 'accent1',
+        KC_DEL: 'accent1',
+        KC_END: 'accent1',
+        KC_PGDN: 'accent1',
+        KC_NLCK: 'accent1',
+        KC_PSLS: 'accent1',
+        KC_PAST: 'accent1',
+        KC_PMNS: 'accent1',
+        KC_PPLS: 'accent1',
+        KC_PCMM: 'accent1',
+        KC_PEQL: 'accent1',
+        KC_LSFT: 'accent2',
+        KC_LCTL: 'accent2',
+        KC_LGUI: 'accent2',
+        KC_LALT: 'accent2',
+        KC_RALT: 'accent2',
+        KC_RGUI: 'accent2',
+        KC_APP: 'accent2',
+        KC_RCTL: 'accent2',
+        KC_RSFT: 'accent2',
+        KC_UP: 'accent2',
+        KC_LEFT: 'accent2',
+        KC_DOWN: 'accent2',
+        KC_RGHT: 'accent2',
+        MO(layer): 'accent3'
     }
 };
--- src/components/colorways/gmk/nines.js
+++ src/components/colorways/gmk/nines.js
@@ -1,9 +1,9 @@
 export default {
     name: 'gmk-nines',
     override: {
-        KC_ESC: 'esc', // Esc
-        KC_GESC: 'esc', // Esc
-        KC_ENT: 'enter', // Enter
-        KC_PENT: 'enter' // Numpad Enter
+        KC_ESC: 'esc',
+        KC_GESC: 'esc',
+        KC_ENT: 'enter',
+        KC_PENT: 'enter'
     }
 };
