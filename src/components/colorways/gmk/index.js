0707ede1e38e3c340e2b65c42e3d637d2872bab4
Sendy YK <mr@sendyyk.com>
2020-09-26 00:22:08 +0700

    Remove GMK Metropolis colorway

--- src/components/colorways/gmk/index.js
+++ src/components/colorways/gmk/index.js
@@ -14,7 +14,6 @@ import gmk_hammerhead_light from './hammerhead_light';
 import gmk_jamon from './jamon';
 import gmk_merlin from './merlin';
 import gmk_metaverse from './metaverse';
-import gmk_metropolis from './metropolis';
 import gmk_mizu from './mizu';
 import gmk_nautilus from './nautilus';
 import gmk_nines from './nines';
@@ -46,7 +45,6 @@ export default [
   gmk_jamon,
   gmk_merlin,
   gmk_metaverse,
-  gmk_metropolis,
   gmk_mizu,
   gmk_nautilus,
   gmk_nines,
