178807b6f3ee966f1a8e931d650d06bc0e03149c
Sendy YK <mr@sendyyk.com>
2020-10-08 04:14:37 +0700

    Add GMK Keyset colorways

---
+++ src/components/colorways/gmk/phosphorous.js
@@ -0,0 +1,10 @@
+export default {
+  name: 'gmk-phosphorous',
+  override: {
+    KC_ESC: 'accent',
+    KC_GESC: 'accent',
+    KC_P: 'accent',
+    KC_ENT: 'accent',
+    KC_PENT: 'accent'
+  }
+};
