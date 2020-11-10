a46a538f92d188fdc78c0dfbad8aeef1b866d26e
Garret G <45295190+The-Royal@users.noreply.github.com>
2020-11-10 16:32:49 -0600

    Add Configurator Keyset Theme "GMK Fro.Yo" (#836)
    * add fro.yo keyset theme files and documentation
    
    * Update fro_dot_yo.js
    
    * Apply suggestions from code review
    
    * Update src/components/Main.vue
    
    * Update src/scss/colorways.scss

---
+++ src/components/colorways/gmk/fro_dot_yo.js
@@ -0,0 +1,23 @@
+export default {
+  name: 'gmk-fro-dot-yo',
+  override: {
+    KC_TAB: 'blu',
+    KC_BSPC: 'rdp',
+    KC_LGUI: 'blu',
+    KC_RGUI: 'blu',
+    KC_CAPS: 'ppl',
+    KC_ENT: 'grn',
+    KC_PENT: 'grn',
+    KC_LALT: 'grn',
+    KC_RALT: 'grn',
+    KC_LSFT: 'ylw',
+    KC_RSFT: 'ylw',
+    KC_LCTL: 'rdp',
+    KC_RCTL: 'rdp',
+    KC_APP: 'ppl',
+    KC_INS: 'rdp',
+    'MO(layer)': 'ppl',
+    KC_ESC: 'rdp',
+    KC_GESC: 'rdp'
+  }
+};
