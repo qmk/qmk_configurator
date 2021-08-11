7d09ea871301b0f022dfa978b82a1f7ad5ecbe30
Yan-Fa Li <yanfali@gmail.com>
2021-08-10 17:16:47 -0700

    Start of Layer Editor

---
+++ src/components/Layer.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="layer">
+    <div>{{ layer.id }}</div>
+    <div>
+      <button>
+        <font-awesome-icon size="lg" fixed-width :icon="arrowUp" />
+      </button>
+    </div>
+    <div>
+      <button>
+        <font-awesome-icon size="lg" fixed-width :icon="arrowDown" />
+      </button>
+    </div>
+    <div>
+      <button><font-awesome-icon size="lg" fixed-width :icon="copy" /></button>
+    </div>
+    <div>
+      <button><font-awesome-icon size="lg" fixed-width :icon="paste" /></button>
+    </div>
+    <div>
+      <button><font-awesome-icon size="lg" fixed-width :icon="trash" /></button>
+    </div>
+    <div>
+      {{ layerSummary }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'layer',
+  props: {
+    layer: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      arrowUp: 'arrow-up',
+      arrowDown: 'arrow-down'
+    };
+  },
+  computed: {
+    layerSummary() {
+      return 'layer summary goes here';
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.layer {
+  display: flex;
+}
+</style>
---
+++ src/components/LayerEditor.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <layer :key="layer.id" v-for="layer in layers">{{ layer.id }}</layer>
+  </div>
+</template>
+<script>
+import Layer from 'Layer';
+export default {
+  name: 'layer-editor',
+  components: {
+    Layer
+  },
+  props: {
+    layers: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
+<style lang="scss" scoped></style>
