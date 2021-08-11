7f8159ee2fbb5ec1ee7558ee91aac9e65689ce7d
Yan-Fa Li <yanfali@gmail.com>
2021-08-10 17:16:47 -0700

    Use flexbox in both directions

--- src/components/LayerEditor.vue
+++ src/components/LayerEditor.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <layer :key="layer.id" v-for="layer in layers">{{ layer.id }}</layer>
+    <layer :layer="layer" :key="layer.id" v-for="layer in layers">{{
+      layer.id
+    }}</layer>
   </div>
 </template>
 <script>
@@ -18,4 +20,9 @@ export default {
   }
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.layers {
+  display: flex;
+  flex-direction: column;
+}
+</style>
