5220a17dfbf4f9d0d8160ada749262eb0dfd9085
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-08-26 12:51:46 -0700

    move xd60/rev3 default keymap to xiudi/xd60/rev3

--- src/remap.js
+++ src/remap.js
@@ -1,4 +1,5 @@
 const lookup = {
+'xd60/rev3': { target: 'xiudi/xd60/rev3' },
 'xd60/rev2': { target: 'xiudi/xd60/rev2' },
 'xd004/v1': { target: 'xiudi/xd004/v1' },
 'xd002': { target: 'xiudi/xd002' },
