40a54c033abd121a411cad9e19647d9982ab0a7b
James Young <18669334+noroadsleft@users.noreply.github.com>
2021-08-26 14:25:51 -0700

    move handwired/p1800fl default keymap to team0110/p1800fl

--- src/remap.js
+++ src/remap.js
@@ -1,4 +1,5 @@
 const lookup = {
+'handwired/p1800fl': { target: 'team0110/p1800fl' },
 'gmmk/pro': { target: 'gmmk/pro/ansi' },
 'dztech/volcano660': { target: 'ilumkb/volcano660' },
 'durgod/k320': { target: 'durgod/k3x0/k320/base' },
