4026300dca4229e589d0e28abcc5ef5d57be5056
alin m elena <alin@elena.space>
2022-01-18 22:24:51 +0000

    try to add mt3 colourway
    Author: alin m elena <alin@elena.space>

---
+++ src/components/colorways/mt3/dev_tty_teal.js
@@ -0,0 +1,6 @@
+export default {
+  name: 'mt3-dev-tty-teal',
+  override: {
+  BL_STEP: 'mod'
+  }
+};
---
+++ src/components/colorways/mt3/index.js
@@ -0,0 +1,7 @@
+import mt3_dev_tty_base from './dev_tty_base';
+import mt3_dev_tty_teal from './dev_tty_teal';
+import mt3_dev_tty_ortho from './dev_tty_ortho';
+import mt3_susuwatari_base from './susuwatari_base';
+import mt3_susuwatari_ortho from './susuwatari_ortho';
+
+export default [mt3_dev_tty_base, mt3_dev_tty_teal, mt3_dev_tty_ortho, mt3_susuwatari_base, mt3_susuwatari_ortho];
