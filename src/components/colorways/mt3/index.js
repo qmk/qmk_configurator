bfcf9a3f72ed03c3084eeefbb5247d3ad540a2e8
alin m elena <alinm.elena@gmail.com>
2022-01-19 21:44:12 +0000

    fix lint 2

--- src/components/colorways/mt3/index.js
+++ src/components/colorways/mt3/index.js
@@ -4,4 +4,10 @@ import mt3_dev_tty_ortho from './dev_tty_ortho';
 import mt3_susuwatari_base from './susuwatari_base';
 import mt3_susuwatari_ortho from './susuwatari_ortho';
 
-export default [mt3_dev_tty_base, mt3_dev_tty_teal, mt3_dev_tty_ortho, mt3_susuwatari_base, mt3_susuwatari_ortho];
+export default [
+  mt3_dev_tty_base,
+  mt3_dev_tty_teal,
+  mt3_dev_tty_ortho,
+  mt3_susuwatari_base,
+  mt3_susuwatari_ortho
+];
