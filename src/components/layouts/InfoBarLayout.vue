61b2e88612b32a4ea38ffe95cba6fc04efc32d38
Yan-Fa Li <yanfali@gmail.com>
2022-01-20 11:55:29 -0800

    Prototype the Layout components
     - I may need to rip the whole infrastructure down as the existing style
       sheet is currently dominant
     - overall the slots and templates seem to work, but there are issues
       where the child components are being used correctly for sizing. It
       could be their positioning.

---
+++ src/components/layouts/InfoBarLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid grid-cols-infobar">
+    <div class="text-left"><slot name="branding" /></div>
+    <div class=""><slot name="service" /></div>
+    <div class=""><slot name="help" /></div>
+    <div class=""><slot name="settings" /></div>
+  </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'info-bar-layout',
+  setup() {}
+});
+</script>
---
+++ src/components/redesign/InfoBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <info-bar-layout>
+    <template #branding>QMK Configurator</template>
+    <template #service>Service ready</template>
+    <template #help>
+      <font-awesome-icon icon="question-circle" size="lg" />
+    </template>
+    <template #settings>
+      <font-awesome-icon icon="cog" size="lg" />
+    </template>
+  </info-bar-layout>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api';
+import InfoBarLayout from '@/components/layouts/InfoBarLayout.vue';
+
+export default defineComponent({
+  components: { InfoBarLayout },
+  name: 'info-bar',
+  setup() {}
+});
+</script>
