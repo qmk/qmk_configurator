9289671af4e0d9bdf33c372896c590f6ee112191
Yan-Fa Li <yanfali@gmail.com>
2022-01-20 12:13:44 -0800

    Use grid gap instead of explicit heights

--- windi.config.js
+++ windi.config.js
@@ -5,7 +5,7 @@ module.exports = {
         infobar: 'minmax(50vw, 1fr) minmax(20vw, 1fr) 1fr 1fr'
       },
       gridTemplateRows: {
-        configurator: 'minmax(45vh, 1fr) minmax(45vh, 1fr)'
+        configurator: '1fr 1fr'
       },
       colors: {
         'white-panel': '#CCD9F7',
