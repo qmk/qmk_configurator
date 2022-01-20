f74513eff5db00d89803184a6fb5f28ec0301ab6
Yan-Fa Li <yanfali@gmail.com>
2022-01-19 21:00:23 -0800

    Add Windi-css

--- src/main.js
+++ src/main.js
@@ -14,6 +14,9 @@ import '@fontsource/roboto-mono/400.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/montserrat/400.css';
+import 'virtual:windi.css';
+import 'virtual:windi-devtools';
+
 
 Vue.use(VueI18n);
 Vue.use(VTooltip);
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@
 const path = require('path');
 import { defineConfig } from 'vite';
 import { createVuePlugin as vue } from 'vite-plugin-vue2';
+import WindiCSS from 'vite-plugin-windicss';
 import CSV from './scripts/i18n/loader.js';
 
 export default defineConfig({
-  plugins: [vue(), CSV()],
+  plugins: [vue(), CSV(), WindiCSS()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
