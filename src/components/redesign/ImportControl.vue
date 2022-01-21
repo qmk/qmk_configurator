da1715e2f7305fbea5b04bc1ce1673c4459a532f
Yan-Fa Li <yanfali@gmail.com>
2022-01-20 20:30:42 -0800

    ImportControl toggle
     - emit uploadJSON event
     - implement import URL toggle

--- src/components/redesign/ImportControl.vue
+++ src/components/redesign/ImportControl.vue
@@ -1,43 +1,81 @@
 <template>
-  <div
-    class="
-      w-full
-      h-56px
-      p-8px
-      flex flex-row
-      justify-between
-      bg-white-panel
-      rounded-lg
-    "
-  >
-    <button
-      class="
-        btn
-        bg-upload-active
-        text-white
-        hover:bg-upload-focus hover:text-upload-active
-        active:text-black active:bg-upload-active
-      "
-    >
-      Upload .JSON
-    </button>
-    <button
-      class="
-        btn
-        bg-upload-active
-        text-white
-        hover:bg-upload-focus hover:text-upload-active
-        active:text-black active:bg-upload-active
-      "
-    >
-      Import URL
-    </button>
+  <div class="w-full bg-white-panel rounded-lg flex flex-col">
+    <div class="w-full h-56px p-8px flex flex-row justify-between">
+      <button
+        class="
+          btn
+          bg-upload-active
+          text-white
+          hover:bg-upload-focus hover:text-upload-active
+          active:text-black active:bg-upload-active
+        "
+        @click="$emit('uploadJSON')"
+      >
+        Upload .JSON
+      </button>
+      <button
+        class="
+          btn
+          bg-upload-active
+          text-white
+          hover:bg-upload-focus hover:text-upload-active
+          active:text-black active:bg-upload-active
+        "
+        @click="toggleImporting"
+      >
+        Import URL
+      </button>
+    </div>
+    <transition name="slide-fade">
+      <div class="w-full flex flex-row p-2" v-show="importing">
+        <input
+          class="
+            flex-grow
+            rounded-md
+            h-40px
+            bg-white-700
+            placeholder-gray-700
+            pl-16px
+            mw-80
+          "
+          type="text"
+          placeholder="Paste URL here"
+        />
+      </div>
+    </transition>
   </div>
 </template>
 <script>
-import { defineComponent } from '@vue/composition-api';
+import { defineComponent, ref, unref } from '@vue/composition-api';
 
 export default defineComponent({
-  setup() {}
+  emits: ['uploadJSON', 'importJSON'],
+  setup() {
+    let importing = ref(false);
+    const toggleImporting = () => {
+      importing.value = !unref(importing);
+    };
+    return {
+      importing,
+      toggleImporting
+    };
+  }
 });
 </script>
+
+<style scoped>
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .3s ease;
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateY(-20px);
+  opacity: 0;
+}
+</style>
+</style>
--- windi.config.js
+++ windi.config.js
@@ -11,6 +11,7 @@ module.exports = {
       colors: {
         'white-panel': '#CCD9F7',
         'white-app': '#ECF2FF',
+        'white-700': '#B1C1E9',
         'upload-active': '#0075ff',
         'upload-focus': 'rgba(0,117,255,0.2)',
         'upload-disabled': 'rgba(6,7,10,1)'
