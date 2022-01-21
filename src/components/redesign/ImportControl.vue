dfebfb393a60ab0a055cf0c8e10ec589cc79cfdc
Yan-Fa Li <yanfali@gmail.com>
2022-01-20 17:41:30 -0800

    Extract Individual Controls

---
+++ src/components/redesign/ImportControl.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="
+      w-full
+      h-56px
+      p-8px
+      flex flex-row
+      justify-between
+      bg-white-panel
+      rounded-lg
+    "
+  >
+    <button class="btn btn-blue">Upload .JSON</button>
+    <button class="btn btn-blue-2">Import URL</button>
+  </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  setup() {}
+});
+</script>
---
+++ src/components/redesign/PotatoFacts.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="
+      w-full
+      h-84px
+      border-blue-100
+      text-left
+      p-8px
+      border-2 border-solid
+      rounded-lg
+    "
+  >
+    Potato Facts
+  </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  setup() {}
+});
+</script>
