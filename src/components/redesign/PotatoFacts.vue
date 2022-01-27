2b8d7cd10eea0de394fcf958b2fdb12584a3f2f1
Yan-Fa Li <yanfali@gmail.com>
2022-01-26 16:47:25 -0800

    Implement potato facts as a widget
     - sadly because we are on vue2 I can't use the composition API for this
       because the i18n v8 API uses a global on this instance
     - this uses a computed value to display the message, and then we
       observe changes in the index which triggers a re-render
     - forward and backward controls are implemented as per mocks but the
       iconography is from FA

--- src/components/redesign/PotatoFacts.vue
+++ src/components/redesign/PotatoFacts.vue
@@ -8,15 +8,71 @@
       p-8px
       border-2 border-solid
       rounded-lg
+      relative
     "
   >
-    Potato Facts
+    {{ potatoFact }}
+    <div class="absolute bottom-0 right-1 text-indigo-400">
+      <a @click="decrementIdx" i>
+        <font-awesome-icon icon="less-than" size="sm" fixed-width />
+      </a>
+      <a @click="incrementIdx">
+        <font-awesome-icon icon="greater-than" size="sm" fixed-width />
+      </a>
+    </div>
   </div>
 </template>
 <script>
 import { defineComponent } from '@vue/composition-api';
+import random from 'lodash/random';
+import size from 'lodash/size';
 
+let cancelInterval = undefined;
+
+// Because I'm using vue2 I have to use legacy API because
+// i18n API composition only really works for vue3 at this time.
 export default defineComponent({
-  setup() {}
+  props: {
+    delay: {
+      type: Number,
+      default: 12000
+    }
+  },
+  data() {
+    const factCount = size(this.$t('potato'));
+    return {
+      interval: this.delay,
+      intervalHandle: undefined,
+      currentFactIdx: random(1, factCount),
+      factCount
+    };
+  },
+  computed: {
+    potatoFact() {
+      return this.$t('potato.' + this.currentFactIdx);
+    }
+  },
+  mounted() {
+    this.intervalHandle = setInterval(() => {
+      this.incrementIdx();
+    }, this.interval);
+  },
+  destroyed() {
+    cancelInterval(this.intervalHandle);
+  },
+  methods: {
+    incrementIdx() {
+      this.currentFactIdx = (this.currentFactIdx + 1) % this.factCount;
+      if (this.currentFactIdx === 0) {
+        this.currentFactIdx = 1;
+      }
+    },
+    decrementIdx() {
+      this.currentFactIdx = (this.currentFactIdx - 1) % this.factCount;
+      if (this.currentFactIdx <= 0) {
+        this.currentFactIdx = this.factCount - this.currentFactIdx;
+      }
+    }
+  }
 });
 </script>
