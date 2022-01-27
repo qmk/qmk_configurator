bc3ab24f57a88ee344353b0be7061690d3a957d4
Yan-Fa Li <yanfali@gmail.com>
2022-01-26 20:56:23 -0800

    Add xterm.js for console
    it's a really big library, but I do like the features it offers so it
    may be worth it if we can lazy load it. We'll only use a small amount of
    it's functionality, but it does render links quite well.

--- src/components/redesign/ConsoleControl.vue
+++ src/components/redesign/ConsoleControl.vue
@@ -1,19 +1,37 @@
 <template>
-  <div class="rounded-lg bg-cool-gray-50 text-black h-48px relative">
+  <div class="rounded-lg bg-cool-gray-50 text-black min-h-48px p-2 relative">
     <div class="absolute left-3 top-2">
       <font-awesome-icon icon="book-open" size="lg" fixed-width />
     </div>
+    <h4 class="font-bold mt-2">Console</h4>
+    <div ref="terminal" class="pl-2 pr-2"></div>
     <div class="absolute right-3 top-2">
       <font-awesome-icon icon="expand-alt" size="lg" fixed-width />
     </div>
-    <h4 class="font-bold mt-2">Console</h4>
   </div>
 </template>
 <script>
 import { defineComponent } from '@vue/composition-api';
-
+import { Terminal } from 'xterm';
+import { WebLinksAddon } from 'xterm-addon-web-links';
+import { FitAddon } from 'xterm-addon-fit';
 export default defineComponent({
   name: 'console-control',
-  setup() {}
+  mounted() {
+    this.term.open(this.$refs.terminal);
+    this.fitAddon.fit();
+    this.term.write('hello world! https://config.qmk.fm');
+  },
+  setup() {
+    const term = new Terminal({ rows: 25, cols: 80 });
+    term.options.fontFamily = 'Courier';
+    term.options.theme.background = '#CCD9F7';
+    term.options.theme.foreground = '#333';
+    term.options.scrollback = 400;
+    term.loadAddon(new WebLinksAddon());
+    const fitAddon = new FitAddon();
+    term.loadAddon(fitAddon);
+    return { term, fitAddon };
+  }
 });
 </script>
--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import '@fontsource/roboto-mono/400.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/montserrat/400.css';
+import 'xterm/css/xterm.css';
 import 'virtual:windi.css';
 import 'virtual:windi-devtools';
 import VueCompositionAPI from '@vue/composition-api';
