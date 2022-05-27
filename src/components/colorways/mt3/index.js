edf45b800bddd799d04f9369b336af3b2c7c1c18
alin m elena <alin@elena.space>
2022-05-27 11:19:05 +0100

    add new mt3 colourways and fix dev tty colourway

--- src/components/colorways/mt3/index.js
+++ src/components/colorways/mt3/index.js
@@ -3,11 +3,23 @@ import mt3_dev_tty_teal from './dev_tty_teal';
 import mt3_dev_tty_ortho from './dev_tty_ortho';
 import mt3_susuwatari_base from './susuwatari_base';
 import mt3_susuwatari_ortho from './susuwatari_ortho';
+import mt3_lotr_elvish_base from './lotr_elvish_base';
+import mt3_lotr_elvish_ortho from './lotr_elvish_ortho';
+import mt3_lotr_dwarven_base from './lotr_dwarven_base';
+import mt3_lotr_dwarven_ortho from './lotr_dwarven_ortho';
+import mt3_3277_base from './3277_base';
+import mt3_3277_ortho from './3277_ortho';
 
 export default [
   mt3_dev_tty_base,
   mt3_dev_tty_teal,
   mt3_dev_tty_ortho,
   mt3_susuwatari_base,
-  mt3_susuwatari_ortho
+  mt3_susuwatari_ortho,
+  mt3_lotr_elvish_base,
+  mt3_lotr_elvish_ortho,
+  mt3_lotr_dwarven_base,
+  mt3_lotr_dwarven_ortho,
+  mt3_3277_base,
+  mt3_3277_ortho
 ];
---
+++ src/components/colorways/mt3/lotr_dwarven_base.js
@@ -0,0 +1,8 @@
+export default {
+  name: 'mt3-lotr-dwarven-base',
+  override: {
+    KC_ENT: 'accent',
+    KC_ESC: 'accent',
+    BL_STEP: 'mod'
+  }
+};
