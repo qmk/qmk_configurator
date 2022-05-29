fd80438ce32244297c6c1183c49068e45b35098f
Yan-Fa Li <yanfali@gmail.com>
2022-05-29 09:17:12 -0700

    Experiment with templates and Slots
     - layouts predefined based on N A X redesign

--- src/views/NewHome.vue
+++ src/views/NewHome.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>New Configurator Goes Here</div>
+  <configurator-layout class="h-100">
+    <template #topbar>New Configurator Goes Here</template>
+    <template #application>Application Goes Here</template>
+    <template #footer>Footer Here</template>
+  </configurator-layout>
 </template>
 
 <script>
+import ConfiguratorLayout from '@/components/layouts/ConfiguratorLayout.vue';
 export default {
   name: 'configuratorTwo',
-  components: {},
+  components: { ConfiguratorLayout },
   methods: {}
 };
 </script>
-<style>
-</style>
+<style></style>
