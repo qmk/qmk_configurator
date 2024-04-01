69a7644dba0f608e6201f8ea397c49c5c8b602c3
James Young <18669334+noroadsleft@users.noreply.github.com>
2024-04-01 14:35:52 -0700

    Update filenames of JSON keymap exports
    Updates the filenames of JSON keymap exports to include the name of the
    keyboard if the keymap name is given, instead of only the keymap name.

--- src/store/modules/app/getters.js
+++ src/store/modules/app/getters.js
@@ -10,9 +10,11 @@ const getters = {
   filter: (state) => state.filter,
   exportKeymapName: (state) => {
     let exportName = state.keymapName.replace(/[\s/]/g, '_').toLowerCase();
+    let keyboardName = state.keyboard.replace(/[\s/]/g, '_').toLowerCase();
     if (exportName === '') {
-      let keyboardName = state.keyboard.replace(/[\s/]/g, '_').toLowerCase();
       exportName = `${keyboardName}_${state.layout}_mine`.toLowerCase();
+    } else {
+      exportName = `${keyboardName}_${exportName}`.toLowerCase();
     }
     // issue #331 whitelist what we send to API for keymapName and save to disk
     exportName = exportName.replace(/[^a-z0-9_-]/gi, '');
