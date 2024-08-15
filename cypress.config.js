47fa70022b3d7046ebde69bf65c6bbc9d92ce079
Yan-Fa Li <yanfali@gmail.com>
2024-08-15 16:34:35 -0700

    chore: remove trailing commas

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   fixturesFolder: 'tests/fixtures',
@@ -8,14 +8,14 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:5173/',
     specPattern: 'tests/integration/**/*.spec.js',
-    supportFile: false,
+    supportFile: false
   },
   component: {
     setupNodeEvents(on, config) {},
-    specPattern: 'src/**/*.spec.js',
-  },
-})
+    specPattern: 'src/**/*.spec.js'
+  }
+});
