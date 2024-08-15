d4183f3e063dc2bf1e3b1b5264e5a14b984c586c
Yan-Fa Li <yanfali@gmail.com>
2024-08-15 15:49:49 -0700

    wip

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  fixturesFolder: 'tests/fixtures',
+  chromeWebSecurity: false,
+  video: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:5173/',
+    specPattern: 'tests/integration/**/*.spec.js',
+  },
+  component: {
+    setupNodeEvents(on, config) {},
+    specPattern: 'src/**/*.spec.js',
+  },
+})
