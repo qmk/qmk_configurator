62847b22344493ce57fe8ff5cc0f70965286069b
Ryan <fauxpark@gmail.com>
2025-06-22 12:46:08 +1000

    Clean up KB settings and app/media/mouse keycode tabs (#1403)

--- src/store/modules/keycodes/app-media-mouse.js
+++ src/store/modules/keycodes/app-media-mouse.js
@@ -6,7 +6,9 @@ export default [
   { name: 'Power', code: 'KC_PWR', title: 'System Power Down' },
   { name: 'Sleep', code: 'KC_SLEP', title: 'System Sleep' },
   { name: 'Wake', code: 'KC_WAKE', title: 'System Wake' },
-  { width: 1250 },
+
+  { width: 500 },
+
   { name: 'Exec', code: 'KC_EXEC', title: 'Execute' },
   { name: 'Help', code: 'KC_HELP', title: 'Help' },
   {
@@ -23,7 +25,22 @@ export default [
   { name: 'Copy', code: 'KC_COPY', title: 'Copy' },
   { name: 'Paste', code: 'KC_PSTE', title: 'Paste' },
   { name: 'Find', code: 'KC_FIND', title: 'Find' },
+
   { width: 0 },
+
+  {
+    name: 'Bright ⏷',
+    code: 'KC_BRID',
+    title: 'Decrease the screen brightness (for laptops)'
+  },
+  {
+    name: 'Bright ⏶',
+    code: 'KC_BRIU',
+    title: 'Increase the screen brightness (for laptops)'
+  },
+
+  { width: 1500 },
+
   {
     name: 'Calc',
     code: 'KC_CALC',
@@ -44,7 +61,9 @@ export default [
     code: 'KC_MYCM',
     title: 'Launch My Computer (Windows)'
   },
+
   { width: 250 },
+
   {
     name: 'Browser Search',
     code: 'KC_WSCH',
@@ -87,40 +106,32 @@ export default [
     title: 'Browser Favorites (Windows)',
     width: 1500
   },
-  {
-    name: 'Brightness Up',
-    code: 'KC_BRIU',
-    title: 'Increase the brightness of screen (Laptop)',
-    width: 1500
-  },
-  {
-    name: 'Brightness Down',
-    code: 'KC_BRID',
-    title: 'Decrease the brightness of screen (Laptop)',
-    width: 1500
-  },
 
   { label: 'Multimedia Keys', width: 'label' },
 
-  { name: 'Prev', code: 'KC_MPRV', title: 'Previous Track' },
-  { name: 'Next', code: 'KC_MNXT', title: 'Next Track' },
-  { name: 'Mute', code: 'KC_MUTE', title: 'Mute Audio' },
-  { name: 'Vol ⏷', code: 'KC_VOLD', title: 'Volume Down' },
-  { name: 'Vol ⏶', code: 'KC_VOLU', title: 'Volume Up' },
-  { name: 'Media Stop', code: 'KC_MSTP', title: 'Media Stop' },
-  { name: 'Play', code: 'KC_MPLY', title: 'Play/Pause' },
-  { width: 250 },
+  { name: 'Prev Track', code: 'KC_MPRV', title: 'Previous Track' },
   {
     name: 'Rewind',
     code: 'KC_MRWD',
     title: 'Previous Track / Rewind (macOS)'
   },
+  { name: 'Play/Pause', code: 'KC_MPLY', title: 'Play/Pause' },
+  { name: 'Stop', code: 'KC_MSTP', title: 'Media Stop' },
   {
     name: 'FFwd',
     code: 'KC_MFFD',
     title: 'Next Track / Fast Forward (macOS)'
   },
+  { name: 'Next Track', code: 'KC_MNXT', title: 'Next Track' },
+
   { width: 250 },
+
+  { name: 'Mute', code: 'KC_MUTE', title: 'Mute Audio' },
+  { name: 'Vol ⏷', code: 'KC_VOLD', title: 'Volume Down' },
+  { name: 'Vol ⏶', code: 'KC_VOLU', title: 'Volume Up' },
+
+  { width: 250 },
+
   { name: 'Eject', code: 'KC_EJCT', title: 'Eject (macOS)' },
 
   { label: 'Mouse Keys', width: 'label' },
@@ -129,7 +140,9 @@ export default [
   { name: 'Mouse ⏷', code: 'MS_DOWN', title: 'Mouse Cursor Down' },
   { name: 'Mouse ⏴', code: 'MS_LEFT', title: 'Mouse Cursor Left' },
   { name: 'Mouse ⏵', code: 'MS_RGHT', title: 'Mouse Cursor Right' },
+
   { width: 250 },
+
   { name: 'Mouse 1', code: 'MS_BTN1', title: 'Mouse Button 1' },
   { name: 'Mouse 2', code: 'MS_BTN2', title: 'Mouse Button 2' },
   { name: 'Mouse 3', code: 'MS_BTN3', title: 'Mouse Button 3' },
@@ -138,12 +151,16 @@ export default [
   { name: 'Mouse 6', code: 'MS_BTN6', title: 'Mouse Button 6' },
   { name: 'Mouse 7', code: 'MS_BTN7', title: 'Mouse Button 7' },
   { name: 'Mouse 8', code: 'MS_BTN8', title: 'Mouse Button 8' },
+
   { width: 0 },
+
   { name: 'Mouse Whl ⏶', code: 'MS_WHLU', title: 'Mouse Wheel Up' },
   { name: 'Mouse Whl ⏷', code: 'MS_WHLD', title: 'Mouse Wheel Down' },
   { name: 'Mouse Whl ⏴', code: 'MS_WHLL', title: 'Mouse Wheel Left' },
   { name: 'Mouse Whl ⏵', code: 'MS_WHLR', title: 'Mouse Wheel Right' },
+
   { width: 250 },
+
   {
     name: 'Mouse Accel 0',
     code: 'MS_ACL0',
@@ -160,18 +177,22 @@ export default [
     title: 'Set mouse acceleration to 2'
   },
 
-  { label: 'Extra Fn Keys', width: 'label' },
+  { label: 'Extended F-Keys', width: 'label' },
 
   { name: 'F13', code: 'KC_F13' },
   { name: 'F14', code: 'KC_F14' },
   { name: 'F15', code: 'KC_F15' },
   { name: 'F16', code: 'KC_F16' },
+
   { width: 250 },
+
   { name: 'F17', code: 'KC_F17' },
   { name: 'F18', code: 'KC_F18' },
   { name: 'F19', code: 'KC_F19' },
   { name: 'F20', code: 'KC_F20' },
+
   { width: 250 },
+
   { name: 'F21', code: 'KC_F21' },
   { name: 'F22', code: 'KC_F22' },
   { name: 'F23', code: 'KC_F23' },
